8339a95ab917d341c78dc32121faa6c5351c76b4
João Carlos <joaocarlos.losfe@gmail.com>
2020-11-10 13:47:34 -0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+namespace Simple_note
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lbl_numero_dia_do_mes.Text = DateTime.Now.ToString("dd");
+            lbl_nome_do_mes_abreviado.Text = DateTime.Now.ToString("MMM");
+            lbl_ano.Text = DateTime.Now.ToString("yyyy");
+            lbl_nome_do_dia_da_semana.Text = DateTime.Now.ToString("dddd");
+
+            if( ! File.Exists("data.dat"))
+            {
+                File.Create("data.dat");
+            }
+            else
+            {
+                string[] dados = File.ReadAllLines("data.dat");
+
+                foreach(var dado in dados)
+                {
+                    if(dado!= "" || dado != "\n")
+                    {
+                        listview.Items.Add(dado);
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            input_mensage_window input_window = new input_mensage_window();
+            input_window.ShowDialog();
+
+            if (input_window.mensage != null && input_window.mensage != "")
+            {
+                listview.Items.Add(DateTime.Now.ToString("g") + ": " + input_window.mensage);
+
+                File.AppendAllText("data.dat", DateTime.Now.ToString("g") + ": " + input_window.mensage + "\n");
+
+                input_window.mensage = null;
+            }
+                
+        }
+    }
+}
---
+++ input_mensage_window.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Windows;
+
+namespace Simple_note
+{
+    /// <summary>
+    /// Lógica interna para input_mensage_window.xaml
+    /// </summary>
+    public partial class input_mensage_window : Window
+    {
+        public string mensage = null;
+        public input_mensage_window()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mensage = txtbox_mensage.Text;
+            this.Close();
+        }
+    }
+}
